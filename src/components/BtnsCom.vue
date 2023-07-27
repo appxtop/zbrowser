72017e04a0dae403f1d7f25b3655b4d47ff12f61
zwypc <799378666@qq.com>
2023-07-27 16:35:53 +0800

    ...

--- src/components/BtnsCom.vue
+++ src/components/BtnsCom.vue
@@ -1,8 +1,31 @@
 <template>
     <div class="btns">
-        <span class="item min" @click="min()">—</span>
-        <span class="item max" @click="max()">□</span>
-        <span class="item close" @click="close()">✕</span>
+        <a class="item min" @click="min()">
+            <svg t="1690443944988" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                p-id="9551" width="200" height="200">
+                <path
+                    d="M930.355 551.424H94.218c-23.987 0-43.975-17.408-43.975-39.424 0-21.504 19.42-39.424 43.975-39.424h835.564c23.987 0 43.975 17.408 43.975 39.424 0.006 22.016-19.415 39.424-43.402 39.424z"
+                    p-id="9552">
+                </path>
+            </svg>
+        </a>
+        <a class="item max" @click="max()">
+            <svg t="1690443865901" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                p-id="7561" width="200" height="200">
+                <path
+                    d="M800 128h-576C172.8 128 128 172.8 128 224v576c0 51.2 44.8 96 96 96h576c51.2 0 96-44.8 96-96v-576c0-51.2-44.8-96-96-96z m32 672c0 19.2-12.8 32-32 32h-576c-19.2 0-32-12.8-32-32v-576c0-19.2 12.8-32 32-32h576c19.2 0 32 12.8 32 32v576z"
+                    p-id="7562">
+                </path>
+            </svg>
+        </a>
+        <a class="item close" @click="close()">
+            <svg t="1690444543659" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                p-id="10519" width="200" height="200">
+                <path
+                    d="M556.8 512L832 236.8c12.8-12.8 12.8-32 0-44.8-12.8-12.8-32-12.8-44.8 0L512 467.2l-275.2-277.333333c-12.8-12.8-32-12.8-44.8 0-12.8 12.8-12.8 32 0 44.8l275.2 277.333333-277.333333 275.2c-12.8 12.8-12.8 32 0 44.8 6.4 6.4 14.933333 8.533333 23.466666 8.533333s17.066667-2.133333 23.466667-8.533333L512 556.8 787.2 832c6.4 6.4 14.933333 8.533333 23.466667 8.533333s17.066667-2.133333 23.466666-8.533333c12.8-12.8 12.8-32 0-44.8L556.8 512z"
+                    p-id="10520"></path>
+            </svg>
+        </a>
     </div>
 </template>
 
@@ -39,26 +62,49 @@ function close() {
         flex: 1;
         text-align: center;
 
+        svg {
+            width: 15px;
+            height: 15px;
+            fill: #000000;
+        }
+
         &:hover {
             background: #d0d2d5;
             color: white;
         }
 
+        &:active {
+            background: #c3c6cb;
+        }
+
         &.min {
-            font-size: 8px;
-            font-weight: bold;
+            svg {
+                width: 11px;
+            }
         }
 
         &.max {
-            font-size: 18px;
-            line-height: 26px;
+            svg {
+                width: 14px;
+                height: 14px;
+                fill: #000000;
+            }
         }
 
         &.close {
-            font-size: 15px;
+
 
             &:hover {
+                svg {
+                    fill: white;
+                }
+
                 background-color: red;
+            }
+
+            &:active {
+                background-color: #e36571;
+                transition: background-color .1s;
             }
         }
     }
--- src/components/TabsCom.vue
+++ src/components/TabsCom.vue
@@ -9,7 +9,13 @@
                     <img v-if="tab.favicons && tab.favicons[0]" class="favicon" :src="tab.favicons[0]" />
                 </div>
                 <div class="title">{{ tab.title }}</div>
-                <div class="close" @click="removeTab(tab.id)">
+                <div class="close zcenter" @click="removeTab(tab.id)">
+                    <svg t="1690403321771" class="icon" viewBox="0 0 1024 1024" version="1.1"
+                        xmlns="http://www.w3.org/2000/svg" p-id="2611" width="200" height="200">
+                        <path
+                            d="M753.365333 843.861333a64 64 0 0 0 90.496-90.496L602.496 512l241.365333-241.365333a64 64 0 0 0-90.496-90.496L512 421.504 270.634667 180.138667a64 64 0 1 0-90.496 90.496L421.504 512l-241.365333 241.365333a64 64 0 0 0 90.496 90.496L512 602.496l241.365333 241.365333z"
+                            fill="#333333" p-id="2612"></path>
+                    </svg>
                 </div>
                 <div class="splite" :class="{
                     show: state.tabIndex !== index && index + 1 != state.tabIndex
@@ -18,7 +24,18 @@
                 </div>
             </div>
         </template>
-        <div class="row btn_add zcenter" @click="addTab()" />
+        <div class="row btn_add zcenter" @click="addTab()">
+            <svg t="1690403514168" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                p-id="4415" width="200" height="200">
+                <path
+                    d="M948.313043 442.991304l-369.530435 0 0-369.530435c0-37.843478-31.165217-69.008696-69.008696-69.008696-37.843478 0-69.008696 31.165217-69.008696 69.008696l0 369.530435-369.530435 0c-37.843478 0-69.008696 31.165217-69.008696 69.008696 0 37.843478 31.165217 69.008696 69.008696 69.008696l369.530435 0 0 369.530435c0 37.843478 31.165217 69.008696 69.008696 69.008696 37.843478 0 69.008696-31.165217 69.008696-69.008696l0-369.530435 369.530435 0c37.843478 0 69.008696-31.165217 69.008696-69.008696C1017.321739 474.156522 988.382609 442.991304 948.313043 442.991304z"
+                    fill="#383b3e" p-id="4416">
+                </path>
+            </svg>
+        </div>
+
+
+
     </div>
 </template>
 <script lang="ts" setup>
@@ -153,13 +170,14 @@ addTab();
             &>.close {
                 position: absolute;
                 right: 8px;
-                border-radius: 100%;
+                border-radius: 50%;
                 width: 15px;
                 height: 15px;
-                background-image: url("../assets/close.svg");
-                background-size: 11px;
-                background-position: center center;
-                background-repeat: no-repeat;
+
+                svg {
+                    width: 12px;
+                    height: 12px;
+                }
 
                 &:hover {
                     background-color: #cfd1d4;
@@ -179,10 +197,12 @@ addTab();
             border-radius: 50%;
             width: 28px;
             height: 28px;
-            background-image: url("../assets/add.svg");
-            background-size: 13px;
-            background-position: center center;
-            background-repeat: no-repeat;
+
+            svg {
+                width: 13px;
+                height: 13px;
+            }
+
             transition: background-color 0.3s ease;
 
             &:hover {
